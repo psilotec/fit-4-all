5007e1f80feb62da89476054098d5d8165c781b5
Scott DeVito <scottadevito@gmail.com>
2017-08-07 18:20:19 -0400

    Add Sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,5 @@ module.exports = {
   siteMetadata: {
     title: `Frascati Fit 4 All`,
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-sass`],
 };
