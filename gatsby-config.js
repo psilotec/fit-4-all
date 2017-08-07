fac1734a486b3598bbef024e2bfacf92b2ef0cea
Scott DeVito <scottadevito@gmail.com>
2017-08-07 18:14:26 -0400

    Gatsby init

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: `Frascati Fit 4 All`,
+  },
+  plugins: [`gatsby-plugin-react-helmet`],
+};
---
+++ src/pages/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Footer = () => {
+  <div>
+    <h1>This is the footer</h1>
+  </div>;
+};
+
+export default Footer;
