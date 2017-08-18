7ec326bf2534693c35e33ced97b43f3e1eb01704
Scott DeVito <scottadevito@gmail.com>
2017-08-18 19:58:06 -0400

    Create Headroom header

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,35 +3,44 @@ import PropTypes from 'prop-types';
 import Link from 'gatsby-link';
 import Helmet from 'react-helmet';
 import Footer from '../pages/footer';
+import Headroom from 'react-headroom';
 
 import './index.scss';
 
 const Header = () =>
-  <div
-    style={{
-      background: 'rebeccapurple',
-      marginBottom: '1.45rem',
-    }}
-  >
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
+  <div>
+    <Headroom>
+      <div
+        style={{
+          background: '#8D8C8C',
+          fontFamily: 'Work Sans,sans-serif',
+          boxShadow: 'rgba(0, 0, 0, 0.25) 1px 1px',
+          height: '75px',
+          fontSize: '3em',
+          color: 'white',
+        }}
+      >
+        <div
           style={{
-            color: 'white',
-            textDecoration: 'none',
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '1.45rem 1.0875rem',
           }}
         >
-          Frascati Fit 4 All
-        </Link>
-      </h1>
-    </div>
+          <h1 style={{ margin: 0 }}>
+            <Link
+              to="/"
+              style={{
+                color: 'white',
+                textDecoration: 'none',
+              }}
+            >
+              Frascati Fit 4 All
+            </Link>
+          </h1>
+        </div>
+      </div>
+    </Headroom>
   </div>;
 
 const TemplateWrapper = ({ children }) =>
