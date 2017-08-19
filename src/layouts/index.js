6cbb087c724fc9bc87dd85203ee81123ac307974
Scott DeVito <scottadevito@gmail.com>
2017-08-18 21:06:11 -0400

    Add content to Index page

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -46,7 +46,7 @@ const Header = () =>
 const TemplateWrapper = ({ children }) =>
   <div>
     <Helmet
-      title="Gatsby Default Starter"
+      title="Frascati Fit 4 All"
       meta={[
         { name: 'description', content: 'Sample' },
         { name: 'keywords', content: 'sample, something' },
@@ -56,9 +56,7 @@ const TemplateWrapper = ({ children }) =>
     <div
       style={{
         margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
+        padding: '0',
       }}
     >
       {children()}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,32 @@
-import React from 'react'
-import Link from 'gatsby-link'
+import React from 'react';
+import Link from 'gatsby-link';
 
 const IndexPage = () =>
-  <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
-  </div>
+  <div className="main-content">
+    <div className="hero">
+      <p>Hero</p>
+    </div>
 
-export default IndexPage
+    <div className="white-section" />
+
+    <div className="accent-section">
+      <div className="card-light">
+        Check this out
+        <button className="success-button">Click here</button>
+      </div>
+      <div className="card-light">Something else</div>
+    </div>
+
+    <div className="white-section" />
+    <div className="accent-section" />
+    <div className="white-section" />
+  </div>;
+
+// <div>
+//   <h1>Hi people</h1>
+//   <p>Welcome to your new Gatsby site.</p>
+//   <p>Now go build something great.</p>
+//   <Link to="/page-2/">Go to page 2</Link>
+// </div>;
+
+export default IndexPage;
